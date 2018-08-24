485a27de021ce2200a036be21bed0681dd6f6a27
W0LFYY <ethanmadrillrivera@gmail.com>
2018-08-24 10:26:09 -0600

    better one

--- palindrome.rb
+++ palindrome.rb
@@ -7,22 +7,22 @@
 
 input = 0
 while input != "quit"
-  puts "=" * 45
+    puts "=" * 45
   puts "Is it a palindrome? Type it in to find out or type 'quit' to exit."
   puts "=" * 45
   input = gets.downcase.strip.gsub(" ", "") 
-  reverse = input.chars.sort_by.with_index {|_, i| -i}.join
-    if input == reverse
+    reverse = input.chars.sort_by.with_index {|_, i| -i}.join
+      if input == reverse
         puts "=" * 45
       puts "That is a palindrome."
-      puts "=" * 45
+           puts "=" * 45
     elsif input == "quit"
         puts "=" * 45
       puts "Goodbye!"
       puts "=" * 45
-    else
+        else
         puts "=" * 45
       puts "That is not a palindrome."
       puts "=" * 45
     end
-end
+    end
